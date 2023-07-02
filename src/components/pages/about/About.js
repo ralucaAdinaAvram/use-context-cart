5bccd56cb8a48009eb66b3b33fc5b75810a10eef
ralucaAdinaAvram <ralucaadinaavram@yahoo.com>
2023-07-02 21:10:23 +0100

    added a paragraph

--- src/components/pages/about/About.js
+++ src/components/pages/about/About.js
@@ -18,6 +18,7 @@ const About = () => {
                      Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi 
                      ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in
                       voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint 
+                    occaecat cupidatat non proident.Excepteur sint 
                     occaecat cupidatat non proident.</p>
             </div>
         </div>
